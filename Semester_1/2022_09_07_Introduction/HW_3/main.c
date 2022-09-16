b9ae9056a8d6167098546cba3bfac0e8e5849d86
GitRadium <gidradiumcode@gmail.com>
2022-09-16 17:41:02 +0300

    2022.09.07 HW 3 fix

--- Semester_1/2022_09_07_Introduction/HW_3/main.c
+++ Semester_1/2022_09_07_Introduction/HW_3/main.c
@@ -2,41 +2,32 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-int devide(int a, int b) {
-    bool isNegative = (a < 0) ^ (b < 0);
-    a = abs(a);
-    b = abs(b);
-
+int divide(int dividend, int divisor) {
     int quotient = 0;
-    while ((quotient + 1) * b <= a) {
-        quotient++;
+    int remainder = dividend;
+    while (remainder < 0 || remainder > abs(divisor))
+    {
+        quotient += ((dividend < 0) ^ (divisor < 0)) ? -1 : 1;
+        remainder = dividend - divisor * quotient;
     }
-
-    if (isNegative) {
-        if (quotient * b < a) {
-            quotient++;
-        }
-
-        quotient = -quotient;
-    }
-
+    
     return quotient;
 }
 
 int main(void) {
-    printf("a = ");
-    int a = 0;
-    scanf("%d", &a);
+    printf("dividend = ");
+    int dividend = 0;
+    scanf("%d", &dividend);
 
-    printf("b = ");
-    int b = 0;
-    scanf("%d", &b);
-    if (b == 0) {
+    printf("divisor = ");
+    int divisor = 0;
+    scanf("%d", &divisor);
+    if (divisor == 0) {
         printf("Error! Can't divide by 0.\n");
         return 0;
     }
 
-    printf("a / b = %d\n", devide(a, b));
+    printf("dividend / divisor = %d\n", divide(dividend, divisor));
 
     return 0;
 }
