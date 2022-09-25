476f8bacf7f259b167fb82409fb3cc6d125102d1
GitRadium <gidradiumcode@gmail.com>
2022-09-25 16:30:48 +0300

    add countingSort

--- Semester_1/2022_09_14_Testing/HW_3/main.c
+++ Semester_1/2022_09_14_Testing/HW_3/main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <time.h>
+#include <limits.h>
 
 void swapInts(int *a, int *b) {
     *a = *a ^ *b;
@@ -9,30 +10,61 @@ void swapInts(int *a, int *b) {
     *a = *a ^ *b;
 }
 
-void bubbleSort(int* array, size_t size) {
+void bubbleSort(int *array, size_t size) {
     if (size < 2) {
         return;
     }
 
     while (true) {
-        bool isSorted = 1;
-        for (int i = 0; i + 1 < size; i++) {
+        bool isSorted = true;
+        for (size_t i = 0; i + 1 < size; i++) {
             if (array[i] > array[i + 1]) {
                 swapInts(&array[i], &array[i + 1]);
                 isSorted = 0;
             }
         }
-        
+
         if (isSorted) {
             break;
         }
     }
 }
 
+void countingSort(int *array, size_t size) {
+    if (size < 2) {
+        return;
+    }
 
+    int min = INT_MAX;
+    int max = INT_MIN;
+    for (size_t i = 0; i < size; i++) {
+        min = (min > array[i] ? array[i] : min);
+        max = (max < array[i] ? array[i] : max);
+    }
 
+    size_t delta = max - min;
+    if (delta == 0) {
+        return;
+    }
 
+    int *mask = calloc(delta + 1, sizeof(int));
 
-int main() {
+    for (size_t i = 0; i < size; i++) {
+        mask[array[i] - min]++;
+    }
+
+    size_t lastEmpty = 0;
+    for (size_t i = 0; i <= delta; i++) {
+        while (mask[i] > 0) {
+            mask[i]--;
+            array[lastEmpty] = i + min;
+            lastEmpty++;
+        }
+    }
+
+    free(mask);
+}
+
+int main(void) {
 
 }
