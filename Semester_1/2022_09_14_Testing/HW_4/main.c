e6023918f4c2d7f44720ceca004050996105b1e6
GitRadium <gidradiumcode@gmail.com>
2022-09-27 15:48:50 +0300

    Add tests

--- Semester_1/2022_09_14_Testing/HW_4/main.c
+++ Semester_1/2022_09_14_Testing/HW_4/main.c
@@ -34,24 +34,42 @@ void halfOfQsort(int *array, const size_t size) {
 }
 
 bool testCorrectness(void) {
-
     bool testsCompleted = true;
+    const size_t testCount = 1000;
+
+    for (size_t testNumber = 0; testNumber < testCount; testNumber++) {
+        const size_t size = rand();
+        int *array = calloc(size, sizeof(int));
+        for (size_t i = 0; i < size; i++) {
+            array[i] = rand() - RAND_MAX / 2;
+        }
+
+        const int pivot = array[0];
+        halfOfQsort(array, size);
+        size_t changeCounter = 0;
+        for (int i = 0; i + 1 < size; i++) {
+            changeCounter += array[i] < pivot && array[i + 1] >= pivot;
+        }
+
+        if (changeCounter > 1) {
+            testsCompleted = false;
+            printf("ERROR! Test %d not passed.\n", testNumber);
+        }
+    }
 
     return testsCompleted;
 }
 
 int main(void) {
+    srand(time(0));
     if (!testCorrectness()) {
         return -1;
     }
 
     printf("Tests completed!\n");
 
-    
-
     const size_t size = 10;
     int *array = calloc(size, sizeof(int));
-    srand(time(0));
     for (size_t i = 0; i < size; i++) {
         array[i] = rand();
     }
