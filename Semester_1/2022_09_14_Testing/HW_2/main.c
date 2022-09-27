7d7cedcf1eaec0d1bca92bcd6f6daa55190bb5cb
GitRadium <gidradiumcode@gmail.com>
2022-09-27 13:04:47 +0300

    last changes

--- Semester_1/2022_09_14_Testing/HW_2/main.c
+++ Semester_1/2022_09_14_Testing/HW_2/main.c
@@ -5,19 +5,25 @@
 
 #define EPSILON 0.00001
 
+struct CorrectTest {
+    double number;
+    int power;
+    double result;
+};
+
 double powLinear(double number, int power) {
     if (power == 0) {
         return 1;
     }
 
-    bool isLessThanZero = (power < 0);
+    const bool isLessThanZero = power < 0;
     power = abs(power);
     double result = 1;
     for (int i = 0; i < power; i++) {
         result *= number;
     }
 
-    return (isLessThanZero ? 1 / result : result);
+    return isLessThanZero ? 1 / result : result;
 }
 
 double powRecursive(double number, int power) {
@@ -29,22 +35,8 @@ double powRecursive(double number, int power) {
     return preresult * preresult * (power % 2 ? (power < 0 ? 1 / number : number) : 1);
 }
 
-struct CorrectTest {
-    double number;
-    int power;
-    double result;
-};
-
-struct CorrectTest createCorrectTest(double number, int power, double result) {
-    struct CorrectTest test;
-    test.number = number;
-    test.power = power;
-    test.result = result;
-    return test;
-}
-
 bool handleTest(struct CorrectTest test, double result, char* functionName) {
-    if (result < test.result - EPSILON || result > test.result + EPSILON) {
+    if (abs(result - test.result) < EPSILON) {
         printf("ERROR! %s(%lf, %d) = %lf, but right answer is between %lf and %lf.\n", 
             functionName, test.number, test.power, result, test.result - EPSILON, test.result + EPSILON);
         return false;
@@ -56,30 +48,30 @@ bool handleTest(struct CorrectTest test, double result, char* functionName) {
 bool testCorrectness(void) {
     const size_t testArraySize = 20;
     struct CorrectTest testArray[20] = {
-        createCorrectTest(1, 1, 1),
-        createCorrectTest(1, 100, 1),
-        createCorrectTest(2, 0, 1),
-        createCorrectTest(100, 0, 1),
+        (struct CorrectTest){1, 1, 1},
+        (struct CorrectTest){1, 100, 1},
+        (struct CorrectTest){2, 0, 1},
+        (struct CorrectTest){100, 0, 1},
 
-        createCorrectTest(2, 1, 2),
-        createCorrectTest(2, 2, 4),
-        createCorrectTest(2, 10, 1024),
-        createCorrectTest(10, 6, 1000000),
+        (struct CorrectTest){2, 1, 2},
+        (struct CorrectTest){2, 2, 4},
+        (struct CorrectTest){2, 10, 1024},
+        (struct CorrectTest){10, 6, 1000000},
 
-        createCorrectTest(0.5, 0, 1),
-        createCorrectTest(0.5, 1, 0.5),
-        createCorrectTest(0.5, 2, 0.25),
-        createCorrectTest(0.5, 10, 0.0009765625),
+        (struct CorrectTest){0.5, 0, 1},
+        (struct CorrectTest){0.5, 1, 0.5},
+        (struct CorrectTest){0.5, 2, 0.25},
+        (struct CorrectTest){0.5, 10, 0.0009765625},
 
-        createCorrectTest(1, -1, 1),
-        createCorrectTest(0.1, -1, 10),
-        createCorrectTest(10, -5, 0.00001),
-        createCorrectTest(1.0/1024, -1, 1024),
+        (struct CorrectTest){1, -1, 1},
+        (struct CorrectTest){0.1, -1, 10},
+        (struct CorrectTest){10, -5, 0.00001},
+        (struct CorrectTest){1.0/1024, -1, 1024},
 
-        createCorrectTest(-1, 1, -1),
-        createCorrectTest(-10, 4, 10000),
-        createCorrectTest(-0.5, -5, -32),
-        createCorrectTest(-0.1, 10, 0.0000000001)
+        (struct CorrectTest){-1, 1, -1},
+        (struct CorrectTest){-10, 4, 10000},
+        (struct CorrectTest){-0.5, -5, -32},
+        (struct CorrectTest){-0.1, 10, 0.0000000001}
     };
 
     bool testsComleted = true;
@@ -96,6 +88,7 @@ int main(void) {
     if (!testCorrectness()) {
         return -1;
     }
+
     printf("Tests completed!\n");
 
     printf("Enter number: ");
