7ce9ac0115e595b0b9a0bc128b9fbe71474bffd1
GitRadium <gidradiumcode@gmail.com>
2022-09-30 17:54:42 +0300

    Add Insertion sorting

--- Semester_1/2022_09_21_Sorting/HW_1/Qsort.c
+++ Semester_1/2022_09_21_Sorting/HW_1/Qsort.c
@@ -1,5 +1,25 @@
 #include "Qsort.h"
 
+void swapInts(int *a, int *b) {
+    *a = *a ^ *b;
+    *b = *a ^ *b;
+    *a = *a ^ *b;
+}
+
 bool Qsort(int *array, size_t size) {
+    if (size <= 1) {
+        return false;
+    }
+
+    if (size <= 10) {
+        for (size_t i = 1; i < size; i++) {
+            for (size_t j = i; j > 0 && array[j] > array[j - 1]; j--) {
+                swapInts(&array[j], &array[j - 1]);
+            }
+        }
+    }
+    
+
+
     return false;
 }
