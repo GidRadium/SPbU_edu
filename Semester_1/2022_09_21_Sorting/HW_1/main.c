6907bb409de92c36f2c35693b805c33a7ede4912
GitRadium <gidradiumcode@gmail.com>
2022-10-02 18:34:39 +0300

    Add tests

--- Semester_1/2022_09_21_Sorting/HW_1/main.c
+++ Semester_1/2022_09_21_Sorting/HW_1/main.c
@@ -4,19 +4,63 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+bool testCorrectness() {
+    const unsigned int seeds[10] = {0, 1, 2, 213123, 123123, 436578, 787878, 5656454, 567878, 254646};
+    const size_t seedsSize = 10;
+    bool testsCompleted = true;
+
+    for (size_t seedIndex = 0; seedIndex < seedsSize; seedIndex++) {
+        srand(seeds[seedIndex]);
+        const size_t size = rand();
+        int *array = calloc(size, sizeof(int));
+        for (size_t i = 0; i < size; i++) {
+            array[i] = rand();
+        }
+
+        qSort(array, size);
+
+        for (size_t i = 0; i + 1 < size; i++) {
+            if (array[i] > array[i + 1]) {
+                testsCompleted = false;
+                printf("Error in test %d: %d > %d", (int)seedIndex, array[i], array[i + 1]);
+            }
+        }
+
+        free(array);
+    }
+    return testsCompleted;
+}
+
 int main(void) {
-    srand(182378);
-    const size_t size = 100;
-    int *array = calloc(size, sizeof(int));
+    if (!testCorrectness()) {
+        printf("Tests failed\n");
+        return -1;
+    }
+    printf("Tests completed!\n");
 
-    for (size_t i = 0; i < size; i++) {
-        array[i] = rand();
+    int arraySize = 0;
+    while (true) {
+        printf("Enter array size: ");
+        scanf("%d", &arraySize);
+        if (arraySize > 0) {
+            break;
+        }
+        printf("ERROR! Array size should be more than 0.\n");
     }
 
-    qSort(array, size);
-    for (size_t i = 0; i < size; i++) {
-        printf("%d\n", array[i]);
+    printf("Enter array:\n");
+    int *array = calloc(arraySize, sizeof(int));
+    for (int i = 0; i < arraySize; i++) {
+        scanf("%d", &array[i]);
     }
-    
+
+    qSort(array, arraySize);
+
+    printf("Sorted array:\n");
+
+    for (int i = 0; i < arraySize; i++) {
+        printf("%d ", array[i]);
+    }
+
     return 0;
 }
