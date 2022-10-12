215d3256534cd911958f1a85a833f6772687585a
GitRadium <gidradiumcode@gmail.com>
2022-10-12 15:58:51 +0300

    copy qSort and Deque, add tests

---
+++ Semester_1/2022_09_28_Structs/HW_2/main.c
@@ -0,0 +1,36 @@
+#include "qSort.h"
+
+#include <stdio.h>
+
+int *readArrayFromFile(const char *fileName, int *readedArraySize) {
+
+}
+
+int main(void) {
+    /*
+    char *fileName = "input.txt";
+    FILE *file = fopen(fileName, "r");
+    if (file == NULL) {
+        printf("ERROR! File not found.\n");
+        return -1;
+    }
+
+    while (!feof (file))
+
+    int *array = malloc(1 * sizeof(int));
+
+    if (temp < 0) {
+        printf("ERROR! Array size can't be less than 0.\n");
+        fclose(file);
+        return -1;
+    }
+
+    size_t size = temp;
+
+    for (size_t i = 0; i < size; i++) {
+        
+    }
+    */
+
+    return 0;
+}
