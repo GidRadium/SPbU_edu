ae98f2bb6cfe46756ef7924d02f9405676dd45f4
GitRadium <gidradiumcode@gmail.com>
2022-10-26 17:09:50 +0300

    Add check function

--- Semester_1/2022_10_12_Stack/HW_2/bracketSequence.c
+++ Semester_1/2022_10_12_Stack/HW_2/bracketSequence.c
@@ -1,23 +1,78 @@
 #include "bracketSequence.h"
 #include <string.h>
 
+char getOpposite(char bracket) {
+    if (bracket == '(') {
+        return ')';
+    } else if (bracket == ')') {
+        return '(';
+    } else if (bracket == '[') {
+        return ']';
+    } else if (bracket == ']') {
+        return '[';
+    } else if (bracket == '}') {
+        return '{';
+    } else if (bracket == '{') {
+        return '}';
+    }
+
+    return '\0';
+}
+
 bool checkBracketSequenceCorrectness(char *bracketSequence, int *errorCode) {
     int stackErrorCode = 0;
     size_t size = strlen(bracketSequence);
     
     Stack *stack = createStack(&stackErrorCode);
     if (stackErrorCode != 0) {
+        if (errorCode != NULL) {
+            *errorCode = stackErrorCode;
+        }
+
         return false;
     }
 
+    bool result = true;
     char bracket = '\0';
+
     for (size_t i = 0; i < size; i++) {
         bracket = bracketSequence[i];
-        if (bracket == '(') {
-            push(stack, '(', &stackErrorCode);
-            //if (stackErrorCode)
+        if (bracket == '(' || bracket == '[' || bracket == '{') {
+            push(stack, (int)bracket, &stackErrorCode);
+        } else if(bracket == ')' || bracket == ']' || bracket == '}') {
+            if (getSize(stack, NULL) == 0) {
+                result = false;
+                break;
+            }
+
+            if (getOpposite(bracket) == (char)getTop(stack, NULL)) {
+                pop(stack, &stackErrorCode);
+            } else {
+                result = false;
+                break;
+            }
+        } else {
+            result = false;
+            break;
+        }
+
+        if (stackErrorCode != 0) {
+            result = false;
+            if (errorCode != NULL) {
+                *errorCode = stackErrorCode;
+            }
+
+            break;
         }
     }
 
-    return false;
+    if (stackErrorCode == 0) {
+        if (getSize(stack, NULL) != 0) {
+            result = false;
+        }
+
+        deleteStack(stack, &stackErrorCode);
+    }
+
+    return result;
 }
