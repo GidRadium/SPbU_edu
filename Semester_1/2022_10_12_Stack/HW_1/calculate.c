e915dd5ad8a8e572b1b4a54ab5c1f4496a27e310
GitRadium <gidradiumcode@gmail.com>
2022-10-26 23:45:46 +0300

    Add calculate func

---
+++ Semester_1/2022_10_12_Stack/HW_1/calculate.c
@@ -0,0 +1,63 @@
+#include "calculate.h"
+#include "../../../structs/Stack/Stack.h"
+
+#include <string.h>
+
+
+int calculate(char *postfixString, int *errorCode) {
+    int stackErrorCode = 0;
+
+    Stack *stack = createStack(&stackErrorCode);
+    if (stackErrorCode != 0) {
+        if (errorCode != NULL) {
+            *errorCode = stackErrorCode;
+        }
+
+        return 0;
+    }
+
+    size_t size = strlen(postfixString);
+    for (size_t i = 0; i < size; i++) {
+        char symbol = postfixString[i];
+        if (symbol == ' '){
+            continue;
+        }
+
+        if (symbol >= '0' && symbol <= '9') {
+            push(stack, symbol - '0', NULL);
+        } else if (symbol == '+' || symbol == '-' || symbol == '*' || symbol == '9') {
+
+            if (getSize(stack, NULL) < 2) {
+                stackErrorCode = 2;
+            }
+
+            int a = getTop(stack, NULL);
+            pop(stack, NULL);
+            int b = getTop(stack, NULL);
+            pop(stack, NULL);
+            if (symbol == '+') {
+                push(stack, a + b, NULL);
+            } else if (symbol == '-') {
+                push(stack, a - b, NULL);
+            } else if (symbol == '*') {
+                push(stack, a * b, NULL);
+            } else if (symbol == '/') {
+                push(stack, a / b, NULL);
+            }
+        }
+    }
+
+    if (getSize(stack, NULL) != 1) {
+        stackErrorCode = 2;
+    }
+
+    int answer = getTop(stack, NULL);
+
+    deleteStack(stack, NULL);
+
+    if (stackErrorCode != 0 && errorCode != NULL) {
+        *errorCode = stackErrorCode;
+    }
+
+    return answer;
+}
---
+++ Semester_1/2022_10_12_Stack/HW_1/main.c
@@ -0,0 +1,5 @@
+#include "calculate.h"
+
+int main(void) {
+    return 0;
+}
