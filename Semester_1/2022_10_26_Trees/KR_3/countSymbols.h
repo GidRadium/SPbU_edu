5c10d9cd9ef8afa39f9f5a7ec0ab91d38950c682
GitRadium <gidradiumcode@gmail.com>
2022-10-26 18:27:43 +0300

    Change comment

--- Semester_1/2022_10_26_Trees/KR_3/countSymbols.h
+++ Semester_1/2022_10_26_Trees/KR_3/countSymbols.h
@@ -1,6 +1,6 @@
 #pragma once
 
-// Function, that counts char symbols in file. If file is not found, 
-// returns -1 if file not found
-// returns 1 if resiltArray is NULL
+// Function, that counts char symbols in file. 
+// Returns -1 if file not found.
+// Returns 1 if resiltArray is NULL.
 void countSymbols(int resultArray[256], const char* filePath, int* errorCode);
