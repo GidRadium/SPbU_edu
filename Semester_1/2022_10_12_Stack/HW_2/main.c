f7857a5d097bce287f7016f0da8b553357390d97
GitRadium <gidradiumcode@gmail.com>
2022-10-26 23:03:55 +0300

    Add empty strings

--- Semester_1/2022_10_12_Stack/HW_2/main.c
+++ Semester_1/2022_10_12_Stack/HW_2/main.c
@@ -5,8 +5,10 @@ int main(void) {
     printf("Enter bracket sequence:\n");
     char string[200];
     scanf("%s", string);
+
     int errorCode = 0;
     bool result = checkBracketSequenceCorrectness(string, &errorCode);
+    
     if (errorCode == 0) {
         printf(result 
             ? "Bracket sequence is correct!\n" 
