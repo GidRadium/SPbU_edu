60c59b9e6cfc354478b3532ad617e3185c7544ca
GidRadium <73718589+GidRadium@users.noreply.github.com>
2022-11-20 18:50:00 +0300

    Update Semester_1/2022_10_26_Trees/KR_3/countSymbols.h
    Co-authored-by: Yurii Litvinov <yurii.litvinov@gmail.com>

--- Semester_1/2022_10_26_Trees/KR_3/countSymbols.h
+++ Semester_1/2022_10_26_Trees/KR_3/countSymbols.h
@@ -1,6 +1,6 @@
 #pragma once
 
-// Function, that counts char symbols in file. 
+// Function that counts char symbols in file. 
 // Returns -1 if file not found.
 // Returns 1 if resiltArray is NULL.
 void countSymbols(int resultArray[256], const char* filePath, int* errorCode);
