f4e91c2954d696c5f58505ca30872acf42e41b8a
GitRadium <gidradiumcode@gmail.com>
2022-11-21 14:44:07 +0300

    Add comments to DynamicArray

--- structs/DynamicArray/DynamicArray.h
+++ structs/DynamicArray/DynamicArray.h
@@ -1,21 +1,31 @@
 #pragma once
 
+// Stored element type
 typedef int DynamicArrayElement;
 
+// Main struct of DynamicArray
 typedef struct DynamicArray DynamicArray;
 
-DynamicArray * createDynamicArray();
+// Allocates mem for DynamicArray. Returns NULL if something goes wrong
+DynamicArray *createDynamicArray();
 
+// Deletes DynamicArray struct
 void deleteDynamicArray(DynamicArray *dArray);
 
+// Clears all elements and frees memmory
 void clear(DynamicArray *dArray);
 
+// Returns DynamicArrayElement by mod(size)
 DynamicArrayElement get(DynamicArray *const dArray, int index);
 
+// Sets DynamicArrayElement by mod(size)
 void set(DynamicArray *const dArray, int index, DynamicArrayElement element);
 
+// Pushes DynamicArrayElement to the back of array. Allocates mem sometimes
 void pushBack(DynamicArray *const dArray, DynamicArrayElement element);
 
+// Deletes last DynamicArrayElement
 void popBack(DynamicArray *const dArray);
 
+// Returns count of stored objects
 size_t getSize(DynamicArray *const dArray);
