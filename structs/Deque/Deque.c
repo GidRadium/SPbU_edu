ff6270e9719e113f2ae0073a1354b2289d82f996
GitRadium <gidradiumcode@gmail.com>
2022-11-23 00:57:26 +0300

    Add create, delete, clear to DQ

--- structs/Deque/Deque.c
+++ structs/Deque/Deque.c
@@ -12,7 +12,50 @@ typedef struct Deque {
     DequeNode *end;
 } Deque;
 
+Deque *createDeque() {
+    Deque *deque = malloc(sizeof(Deque));
+    if (deque == NULL) {
+        return NULL;
+    }
 
+    deque->size = 0;
+    deque->begin = NULL;
+    deque->end = NULL;
+
+    return deque;
+}
+
+void deleteDeque(Deque *deque) {
+    if (deque == NULL) {
+        return;
+    }
+
+    DequeNode *tempNode = deque->begin;
+    while (tempNode != NULL) {
+        deque->begin = deque->begin->next;
+        free(tempNode);
+        tempNode = deque->begin;
+    }
+
+    free(deque);
+    deque = NULL;
+}
+
+void clear(Deque *const deque) {
+    if (deque == NULL) {
+        return;
+    }
+
+    DequeNode *tempNode = deque->begin;
+    while (tempNode != NULL) {
+        deque->begin = deque->begin->next;
+        free(tempNode);
+        tempNode = deque->begin;
+    }
+
+    deque->size = 0;
+    deque->end = NULL;
+}
 
 
 
