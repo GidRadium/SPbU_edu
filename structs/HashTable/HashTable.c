52afb5cd5b67ebf8d0ac5ac47bf4ef9ce09fe4a2
GitRadium <gidradiumcode@gmail.com>
2022-12-07 14:23:21 +0300

    Change HashTable

--- structs/HashTable/HashTable.c
+++ structs/HashTable/HashTable.c
@@ -2,7 +2,18 @@
 
 typedef struct HashTable {
     size_t size;
+    
 };
 
+size_t getHash(HashTableKey key, size_t hashSize) {
+    size_t result = 0;
+    for (size_t i = 0; key[i] != '\0'; i++) {
+        result = (result + key[i]) % hashSize;
+    }
 
+    return result;
+}
 
+HashTable *createHashTable() {
+    
+}
--- structs/HashTable/HashTable.h
+++ structs/HashTable/HashTable.h
@@ -1,10 +1,14 @@
 #pragma once
 
-#include "HashTableTypes.h"
+#include <stdlib.h>
+
+typedef char * HashTableKey;
+
+typedef int HashTableValue;
 
 typedef struct HashTable HashTable;
 
-
+size_t getHash(HashTableKey key);
 
 HashTable *createHashTable();
 
@@ -12,4 +16,6 @@ void deleteHashTable(HashTable *hashTable);
 
 void clear(HashTable *hashTable);
 
+void set(HashTable *hashTable, HashTableKey key, HashTableValue value);
 
+HashTableValue get(HashTable *hashTable, HashTableKey key);
