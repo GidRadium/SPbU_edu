a2fa5399e432d7fae7bfb756e823d9bfc6d4e2ac
GitRadium <gidradiumcode@gmail.com>
2022-12-21 16:51:43 +0300

    scanf -> scanf_s

--- Semester_1/2022_12_21_TEST_3/main.c
+++ Semester_1/2022_12_21_TEST_3/main.c
@@ -4,8 +4,8 @@
 
 int main(void) {
     printf("Enter string (less than 200 symbols):\n");
-    char string[200];
-    scanf("%s", string);
+    char string[200] = {'\0'};
+    scanf_s("%s", string, 200);
     bool result = checkString(string);
     printf("%s\n", (result ? "String is correct!" : "String is NOT correct!"));
     return;
