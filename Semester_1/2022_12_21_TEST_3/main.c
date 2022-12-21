f9866b4446e272db32f7e7070c769dc38172e284
GitRadium <gidradiumcode@gmail.com>
2022-12-21 16:52:11 +0300

    return 0 -_-

--- Semester_1/2022_12_21_TEST_3/main.c
+++ Semester_1/2022_12_21_TEST_3/main.c
@@ -8,5 +8,5 @@ int main(void) {
     scanf_s("%s", string, 200);
     bool result = checkString(string);
     printf("%s\n", (result ? "String is correct!" : "String is NOT correct!"));
-    return;
+    return 0;
 }
